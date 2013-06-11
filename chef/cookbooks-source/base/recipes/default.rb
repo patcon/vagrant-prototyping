f14a394e834d6627b43fb9d1d725962c63630112
Patrick Connolly <patrick@myplanetdigital.com>
2013-06-10 23:53:51 -0400

    Adds 5 retries to gnupg2 pkg resource.

--- chef/cookbooks-source/base/recipes/default.rb
+++ chef/cookbooks-source/base/recipes/default.rb
@@ -4,4 +4,10 @@ when "debian"
   include_recipe "unattended-upgrades"
 when "rhel"
   include_recipe "yum::epel"
+
+  # Often run out of mirrors with this package,
+  # so allow a few retries.
+  package "gnupg2" do
+    retries 5
+  end
 end
